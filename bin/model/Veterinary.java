fd79667601bbace3dc89a70cb08d915ba06ec7b7
Josue Rodriguez Pineda <1005969243@academico.icesi.edu.co>
2019-03-22 09:04:48 -0500

    mejora

--- bin/model/Veterinary.java
+++ bin/model/Veterinary.java
@@ -5,19 +5,21 @@ public class Veterinary{
 	// Atributos
 	
 	public final static int HOS = 1;
+	public final static int ROOMS_MAX = 8;
 	private String name;
 	
-	private ArrayList<Hospitalization> hospi;
 	private ArrayList<ClinicHistory> history;
 	private ArrayList<Client> client;
+	private Room[] rooms;
+	
 	
 	//constructor
 	
 	public Veterinary(String theName){
 		name = theName;
-		hospi = new ArrayList<>();
 		history = new ArrayList<>();
 		client = new ArrayList<>();
+		rooms = new Room[ROOMS_MAX];
 	}
 	//gets y sets
 	public String getName(){
@@ -26,15 +28,13 @@ public class Veterinary{
 	public void setName(String name){
 		this.name = name;
 	}
-	public ArrayList<Hospitalization> getHospi(){
-		return hospi;
-	}
 	public ArrayList<ClinicHistory> getHistory(){
 		return history;
 	}
 	public ArrayList<Client> getClient(){
 		return client;
 	}
+	public 
 	//metodos
 	//Agregar cliente y mascota
 	public void addClient(String name, int id, String address , int phoneNumber, String nameM, String type, int age, double weight){
@@ -47,6 +47,26 @@ public class Veterinary{
 			}
 		}
 	}
+	public String assignPet(Pet pet){
+		String menssage = "";
+		boolean bol = false;
+		for(int i = 0; i < ROOMS_MAX && bol == false; i++){
+			if(rooms[i] != null){
+				rooms[i].setPet(pet);
+				menssage = "La mascota ha sido agregada";
+				bol = true;
+			}
+		}
+		if(bol == false){
+			menssage = "No se pudo agregar la mascota";
+		}
+		return menssage;
+	}
+	public boolean stateHistory(){
+		boolean lu = false;
+		
+		return lu;
+	}	
 
  
 }
